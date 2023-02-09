84ed3df92066f5a29987f3dac40ebe0827c61323
klgibsonjr <klgibsonjr@gmail.com>
2023-02-09 13:03:29 -0800

    create schema.sql and created techblog_db database

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const session = require('express-session');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const Handlebars = require('handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
